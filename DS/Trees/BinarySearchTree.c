bb1163e7cecfc2c607c03c2ebe756bbdedaea224
piyush97 <me@piyushmehta.com>
2017-11-26 20:28:14 +0530

    BST Full DOne

--- DS/Trees/BinarySearchTree.c
+++ DS/Trees/BinarySearchTree.c
@@ -3,7 +3,8 @@
 struct NODE
 {
 	int info;
-	struct NODE* left,right;
+	struct NODE* left;
+	struct NODE* right;
 }*root=NULL;
 void bst(int item)
 {
@@ -47,4 +48,52 @@ void preOrder(struct NODE *root)
 		preOrder(root->left);
 		preOrder(root->right);
 	}
+}
+void postOrder(struct NODE *root)
+{
+	if (root!=NULL)
+	{
+		postOrder(root->left);
+		postOrder(root->right);	
+		printf("%d\n", root->info);
+	}
+}
+void inOrder(struct NODE *root)
+{
+	if (root!=NULL)
+	{
+		inOrder(root->left);
+		printf("%d\n", root->info);
+		inOrder(root->right);	
+	}
+}
+int main()
+{
+	int item;
+	int flag=0;
+	int key;
+	int choice=0;
+	while(choice!=5)
+	{
+		printf("\n1. Create a bst\n 2.preOrder \n3.postOrder \n4.inOrder");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:
+				printf("Enter an item\n");
+				scanf("%d",&item);
+				bst(item);
+				break;
+			case 2:
+				preOrder(root);
+				break;
+			case 3:
+				postOrder(root);
+				break;
+			case 4:
+				inOrder(root);
+				break;
+		}
+	}
+	return 0;
 }
