9be11073b455b9059e1fe949d4f92d004adc0e73
piyush97 <me@piyushmehta.com>
2017-11-26 11:18:55 +0530

    array index out of bound

---
+++ JAVA/Exception Handling/ArrayIndexOutOfBoundExceptionHandling.java
@@ -0,0 +1,19 @@
+public class ArrayIndexOutOfBoundExceptionHandling {
+    public static void main(String[] args) {
+        int a[]=new int[4];
+        try{
+            for(int i=0;i<6;i++)//for exception we are moving index from 0 to 6 but the array size if 4 so it'll throw an exception
+            {
+                a[i]+=i;
+            }
+            for (int value:a)
+            {
+                System.out.println(value);
+            }
+        }
+        catch (Exception e)//i'm writing just Exception coz i don't know which kind of exception it may throw
+        {
+            System.out.println("the exception is "+e);
+        }
+    }
+}
