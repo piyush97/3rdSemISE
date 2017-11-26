754cb7ffbdde83973299d3ff33cf06c2ffb18e70
piyush97 <me@piyushmehta.com>
2017-11-26 12:40:04 +0530

    changes

--- DS/dsPractice/Singly Linked List.c
+++ DS/dsPractice/Singly Linked List.c
@@ -103,3 +103,87 @@ void display()
 		}
 	return;	
 }
+void insertPos(int item, int pos)
+{
+int c=1;
+struct node* newnode;
+newnode=(struct node*)malloc(sizeof(struct node));
+newnode->info=item;
+if(start==NULL && pos==1)
+{
+	newnode->link=NULL;
+	start=newnode;
+	return;
+}	
+else if(start==NULL && pos>1)
+{
+	printf("Invalid position\n");
+}
+else if(start!=NULL && pos==1)
+{
+	newnode->link=startl
+	start=newnode;
+	return;
+}
+struct node* temp=start;
+while(temp!=Null && c!=(pos-1))
+{
+	temp=temp->link;
+	c++;
+}
+if(temp==NULL)
+{
+	printf("Invalid Position");
+	return;
+}
+newnode->link=temp->link;
+temp->link=newnode;
+}
+
+int main()
+{
+		int ch,x,pos;
+	while(ch!=6)
+	{
+		printf("enter choice \n 1 for insertion at begining \n 2 for insertion at any position \n 3 for display \n 4 for insertion at end \n 5 for delete from beinging \n 6 for insertion at any position");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:
+				{printf("Enter the value\n");
+				scanf("%d",&x);
+				InsertBeg(x);
+				break;}
+			
+			case 2:
+				{printf("Enter position and element");
+				scanf("%d %d",&pos,&x);
+				InsertPos(x,pos);
+				break;}
+			
+			case 3:
+				{printf("THe link list is\n");
+				display();
+				break;}
+			
+			case 4:
+				{printf("Enter the element\n");
+				scanf("%d",&x);
+				InsertEnd(x);
+				break;}
+			
+			case 5:
+				{DeleteBegining();
+				break;}
+			case 6:
+				printf("Enter the position \n");
+				scanf("%d",&pos);
+				printf("Enter the element\n");
+				scanf("%d",&x);
+				InsertPos(x,pos);
+				break;
+
+		}
+	}
+}
+	
