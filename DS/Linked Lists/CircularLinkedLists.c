49ef066556fb6787901f67bcb8e90b7a22d130d9
piyush97 <me@piyushmehta.com>
2017-11-27 16:39:34 +0530

    cll

--- DS/Linked Lists/CircularLinkedLists.c
+++ DS/Linked Lists/CircularLinkedLists.c
@@ -1,338 +1,206 @@
 #include <stdio.h>
 #include <stdlib.h>
+struct node{
+int info;
+struct node *link;
+};
+typedef struct node* NODE;
+NODE insert_beg(int item,NODE last){
 
-/**
- *  Self Referential Structure
- *  Members:
- *    -info - holds data
- *    -link - Self referential pointer
- */
-typedef struct node
+    NODE temp;
+    temp=(NODE*)malloc(sizeof(NODE));
+temp->info=item;
+if(last==NULL){
+    temp->link=temp;
+return temp;
+}
+    else{
+temp->link=last->link;
+last->link=temp;
+}
+return last;
+}
+NODE insert_last(int item,NODE last){
+    NODE temp,cur;
+    temp=(NODE*)malloc(sizeof(NODE));
+    temp->info=item;
+if(last==NULL){
+    temp->link=temp;
+last=temp;
+    return last;
+}
+ temp->link=last->link;
+last->link=temp;
+last=temp;
+return last;
+}
+NODE insert_pos(int item,int pos,NODE last)
 {
-	int info;
-	struct node * link;
-} node;
+NODE temp,prev,cur;
+temp=(NODE*)malloc(sizeof(NODE));
+temp->info=item;
 
-typedef node* NODE;
-
-
-/**
- *  Allocates memory dynamically on heap, of size equivalent to size of node structure
- *  @params none
- *  @returns NODE (pointer to type struct node)
- */
-NODE getnode()
+int count=1;
+if(last==NULL && pos==1)
+    {
+temp->link=temp;
+last=temp;
+return last;
+}
+if(last==NULL && pos!=1)
 {
-	NODE x;
-	// Allocate memory on heap for size of node structure
-	x= (NODE) malloc(sizeof(node));
-	// Display a message incase of allocation failure
-	if(x==NULL)printf("Node creation failure!\n");
-	// Return a pointer to the newly created node
-	return x;
+    printf("invalid position\n");
+    return last;
+}
+if(pos==1)
+{
+    temp->link=last->link;
+last->link=temp;
+    return last;
+}
+prev=NULL;
+cur=last->link;
+while(count!=pos && cur!=last)
+{
+        prev=cur;
+    cur=cur->link;
+count++;
+}
+if(count==pos)
+{
+        prev->link=temp;
+   temp->link=cur;
+   return last;
+}
+printf("invalid position\n");
+return last;
+}
+NODE delete_beg(NODE last)
+{
+NODE temp;
+if(last==NULL)
+{
+    printf("list is empty\n");
+    return last;}
+if(last->link==last){
+printf("item deleted is=%d",last->info);
+free(last);
+return NULL;
+}
+temp=last->link;
+printf("deleted node is=%d",temp->info);
+last->link=temp->link;
+free(temp);
+return last;
+}
+NODE delete_last(NODE last){
+NODE prev=NULL;
+if(last==NULL){
+    printf("empty list");
+    return last;
+}
+if(last->link==last){
+printf("deleted itm is=%d",last->info);
+free(last);
+return NULL;
+}
+prev=last->link;
+while(prev->link!=last){
+
+    prev=prev->link;
+    }
+prev->link=last->link;
+last->link=NULL;
+    printf("deleted node is=%d",last->info);
+
+   free(last);
+
+return prev;
+
+}
+NODE delete_pos(NODE last,int pos){
+NODE cur,prev;
+
+if(last==NULL || pos<=0){
+    printf("invalid position\n");
+    return NULL;
+}
+if(last->link==last && pos==1){
+printf("deleted item is=%d",last->info);
+  free(last);
+return NULL;
+}
+cur=last->link;
+prev=last;
+int count=1;
+while(count!=pos && cur!=last){
+    prev=cur;
+    cur=cur->link;
+    count++;
 }
 
-/**
- *  Prints data stored in node passed as parameter
- *  De allocates memory dynamically on heap, of size equivalent to size of node structure
- *  @params temp (pointer to node)
- *  @returns none
- */
-void freenode(NODE temp)
-{
-	// Display contents of node to be deleted
-	printf("Deleting %d\n",temp->info);
-	// Deallocate memory for the node
-	free(temp);
+if(count==pos){
+    prev->link=cur->link;
+
+    if(cur==last){
+        last=prev;
+    }
+     free(cur);
+    return last;
+}
+else if(cur==last){
+printf("position not found\n");
+        return last;
+}
+else{
+printf("valid position not found\n");
+return last;
+}
+}
+void display(NODE last){
+    NODE temp;
+if(last==NULL){
+    printf("linked list is empty\n");
+    return;
+}
+    temp=last->link;
+    while(temp!=last){
+
+    printf("%d ",temp->info);
+        temp=temp->link;
+    }
+printf("%d",last->info);
+
+return;
+
 }
 
-/**
- *  Inserts a node at the begining of the list
- *  @params item (integer) , last (Pointer to last node)
- *  @returns NODE (pointer to last node)
- */
-NODE insert_front(int item, NODE last)
-{
-	NODE temp;
-	// Allocate Memory for new node and set contents to data given by user
-	temp = getnode();
-	temp->info = item;
-	// If list is empty, return pointer to new node as last
-	if(last == NULL)
-	{
-		temp->link = temp;
-		return temp;
-	}
-	// Make the new node's link point to the current first node
-	temp->link = last->link;
-	// Make the last node's link point to newly created node
-	last->link = temp;
-	// Return the last node
-	return last;
+int main(){
+NODE last=NULL;
+int ch,item,pos;
+printf("WELCOME (circular) LINK LIST PROGRAM\n");
+printf("---------------------------------------\n");
+for(;;){
+    printf("enter the choice\n1:insert_front\n2:delete_front\n3:insert_last\n4:delete_last\n5:insert_position\n6:delete_position\n7:display\n");
+    scanf("%d",&ch);
+    switch(ch){
+    case 1:printf("enter the elemnt to insert\n");
+    scanf("%d",&item);
+    last=insert_beg(item,last);break;
+    case 2:last=delete_beg(last);break;
+    case 3:printf("enter the element\n");
+    scanf("%d",&item);
+    last=insert_last(item,last);break;
+    case 4:last=delete_last(last);break;
+    case 5:printf("enter the postion and elemnet\n");
+    scanf("%d%d",&pos,&item);
+    last=insert_pos(item,pos,last);break;
+    case 6:printf("enter the position of elemnt\n");
+    scanf("%d",&pos);
+   last=delete_pos(last,pos);break;
+    case 7:display(last);break;
+    default:exit(0);
+    }
 }
-
-/**
- *  Deletes a node at the begining of the list
- *  @params  last (Pointer to last node)
- *  @returns NODE (pointer to last node)
- */
-NODE delete_front(NODE last)
-{
-	NODE temp;
-	// If list is empty print a message and exit function
-	if(last == NULL)
-	{
-		printf("List Empty!\n");
-		return NULL;
-	}
-	//Otherwise if only one node exists, delete the node and exit the function
-	else if(last->link == last) 
-	{
-		freenode(last);
-		return NULL;
-	}
-	else
-	{
-		// Get the first node
-		temp= last->link;
-		// Set the last node's link to the 2nd node
-		last->link = temp->link;
-		// Delete the first node and return the last node
-		temp->link = NULL;
-		freenode(temp);
-		return last;	
-	}
-}
-
-/**
- *  Inserts a node at the end of the list
- *  @params item (integer) , last (Pointer to last node)
- *  @returns NODE (pointer to last node)
- */
-NODE  insert_rear(int item, NODE last)
-{
-	NODE temp;
-	// Allocate Memory for new node and set contents to data given by user
-	temp = getnode();
-	temp->info = item;
-	// If list is empty, return pointer to new node as last
-	if(last == NULL)
-	{
-		temp->link = temp;
-		return temp;
-	}
-	// Set the link of new node to the first node
-	temp->link = last->link;
-	// Set the last node's link to new node and make it the last node
-	last->link = temp;
-	last = temp;
-	// Return the last node
-	return last;
-}
-
-/**
- *  Deletes a node at the end of the list
- *  @params  last (Pointer to last node)
- *  @returns NODE (pointer to last node)
- */
-NODE delete_rear(NODE last)
-{
-	NODE prev,cur;
-	
-	if(last == NULL)
-	{
-		printf("List Empty!\n");
-		return NULL;
-	}
-	else if(last->link == last)
-	{
-		freenode(last);
-		return NULL;
-	}
-	else
-	{
-		prev = last;
-		cur = last->link;
-		while(cur!=last)
-		{
-			prev= cur;
-			cur = cur->link;
-		}	
-		prev->link = cur->link;
-		cur->link = NULL;
-		freenode(cur);
-		last = prev;
-		return last;
-	}
-}
-
-/**
- *  Inserts a node at a given position in the list
- *  @params last (Pointer to last node),item (integer),pos (integer) 
- *  @returns NODE (pointer to last node)
- */
-NODE insert_pos( NODE last, int pos, int item)
-{
-	int count =1;
- 	NODE prev, cur, temp;
- 	if(pos<1) return last;
- 	temp = getnode();
- 	temp->info = item;
- 	if(last == NULL)// List is empty
- 	{
- 		temp->link = temp;
- 		return temp;
- 	}
- 	if(pos == 1) //insert front
- 	{
- 		temp->link = last->link;
- 		last->link = temp;
- 		return last;
- 	}
- 	prev = NULL; cur = last->link;
- 	while(pos!=count && prev!=last)
- 	{
- 		prev = cur;
- 		cur=cur->link;
- 		count++;
- 	}
- 	if(pos == count)
- 	{
- 		prev->link = temp;
- 		temp->link = cur;
- 		if(prev == last)
- 			last = temp;
- 		return last;
- 		
- 	}
- 	printf("Invalid Position!\n");
- 	return last;
-}
-
-
-/**
- *  Deletes a node at a given position in the list
- *  @params last (Pointer to last node),pos (integer) 
- *  @returns NODE (pointer to last node)
- */
-NODE delete_pos(NODE last, int pos)
-{
-	NODE cur,prev;
-	int count =1;
-	if(last == NULL)
-	{
-		printf("List Empty\n");
-		return last;
-	}
-	if(pos == 1)
-	{
-		cur = last->link;
-		last->link = cur->link;
-		freenode(cur);
-		if(last->link == last) 
-			return NULL;
-		return last;
-	}
-	prev = NULL;
-	cur = last->link;
-	while(prev!=last && count!=pos)
-	{
-		prev= cur;
-		cur= cur->link;
-		count++;
-	}
-	if(prev == last && count!=pos)
-	{
-		printf("Invalid position!\n");
-		return last;
-	}
-	if(count == pos)
-	{
-		prev->link = cur->link;
-		if(cur== last)
-			last = prev;
-		freenode(cur);
-	}
-	return last;
-	
-}
-
-
-/**
- *  Displays all elements in the list by traversing through entire list
- *  @params last (Pointer to last node)
- *  @returns none
- */
-void display(NODE last)
-{
-	NODE temp;
-	if(last == NULL)
-	{
-		printf("List Empty!\n");
-		return;
-	}
-	temp = last->link;
-	while(temp!=last)
-	{
-	  printf("%d -> ", temp->info);
-	  temp=temp->link;
-	}
-	printf("%d\n",last->info);
-}
-
-int main()
-{
-	NODE last = NULL;
-	int data,ch,key,pos;
-	do
-	{
-		printf("1. Insert at front of list\n");		
-		printf("2. Insert at end of list\n");
-		printf("3. Delete first element\n");
-		printf("4. Delete last element\n");
-		printf("5. Display the list\n");
-		printf("6. Insert at a position\n");	
-		printf("7. Delete at position\n");
-		printf("8. Exit\n");
-		printf("Enter your choice:");
-		scanf("%d",&ch);
-		switch(ch)
-		{
-			default:
-				break;
-			case 1: 
-				printf("Enter element to insert at front:");
-				scanf("%d",&data);
-				last = insert_front(data,last);
-				break;
-			case 2: 
-				printf("Enter element to insert at end:");
-				scanf("%d",&data);
-				last = insert_rear(data,last);
-				break;
-			case 3: 
-				last = delete_front(last);
-				break;
-			case 4:
-				last = delete_rear(last);
-				break;
-			case 5:
-				printf("The list contains:\n");
-				display(last);
-				break;	
-			case 6: 
-				printf("Enter position to insert:");
-				scanf("%d",&pos);
-				printf("Enter element to insert at position:");
-				scanf("%d",&data);
-				last = insert_pos(last,pos,data);
-				break;
-			case 7: 
-				printf("Enter position to delete:");
-				scanf("%d",&pos);
-				last = delete_pos(last,pos);
-				break;
-		}
-	}while(ch!=8);
-	return 0;
+return 0;
 }
