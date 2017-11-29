4aa8cded751049c8eec12fcea4e65a0b5ef59e4d
piyush97 <me@piyushmehta.com>
2017-11-29 15:05:04 +0530

    practice

---
+++ DS/dsPractice/bst.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+struct node
+ {
+ 	int info;
+ 	struct node *left,*right;
+ }root=NULL;
+
+ void CreateBST(int item)
+ {
+ 	struct node* newnode;
+ 	newnode=(struct )
+ } 
