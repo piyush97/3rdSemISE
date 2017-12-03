5897a81f26733aabdaf953ce47ee6ff1ead9763b
piyush97 <me@piyushmehta.com>
2017-12-03 10:50:31 +0530

    Expression Tree New Code

--- DS/Trees/ExpressionTree.c
+++ DS/Trees/ExpressionTree.c
@@ -1,85 +1,93 @@
 #include<stdio.h>
-#include<string.h>
-#include<ctype.h>
 #include<stdlib.h>
-#include <math.h>
+#include<ctype.h>
+#include<math.h>
 struct node
 {
-  struct node *left;
-  int info;
-  struct node *right;
+  char info;
+  struct node* llink;
+  struct node* rlink;
 };
-
 typedef struct node* NODE;
-
 NODE getnode()
 {
-	NODE x;
-	// Allocate memory on heap for size of node structure
-	x= (NODE) malloc(sizeof(struct node));
-	// Display a message incase of allocation failure
-	if(x==NULL)printf("Node creation failure!\n");
-	// Return a pointer to the newly created node
-	return x;
+  NODE x=(NODE)malloc(sizeof(struct node));
+  return x;
 }
-
+void display(NODE root,int level)
+{
+  int i;
+  if(root==NULL)return;
+  display(root->rlink,level+1);
+  for(i=0;i<level;i++)printf(" ");
+  printf("%c\n",root->info);
+  display(root->llink,level+1);
+  
+}
 NODE create_tree(char postfix[])
 {
-  NODE temp, st[20];//st = stack
+  NODE temp,st[20];
   int i,k;
   char symbol;
-  for (i=k=0;(symbol=postfix[i])!='\0';i++)
+  for(i=k=0;(symbol=postfix[i])!='\0';i++)
   {
-    temp = getnode();
-    temp->info = symbol;
-    temp->left = temp ->right = NULL;
+    temp=getnode();
+    temp->rlink=temp->llink=NULL;
+    temp->info=symbol;
     if(isalnum(symbol))
     {
-      st[k++]= temp;
+      st[k++]=temp;
+      
     }
     else
     {
-      temp->right = st[--k];
-      temp->left = st[--k];
-      st[k++] = temp;
+      temp->rlink=st[--k];
+      temp->llink=st[--k];
+      st[k++]=temp;
     }
   }
   return st[--k];
 }
-
 float eval(NODE root)
 {
   float num;
   switch(root->info)
   {
-    case '+': return eval(root->left)+ eval(root->right);
-    case '-': return eval(root->left)- eval(root->right);
-    case '/': return eval(root->left)/eval(root->right);
-    case '*': return eval(root->left)*eval(root->right);
+    case '+':return eval(root->llink) + eval(root->rlink);
+    case '-':return eval(root->llink) - eval(root->rlink);
+    case '*':return eval(root->llink) * eval(root->rlink);
+    case '/':return eval(root->llink) / eval(root->rlink);
     case '$':
-    case '^': return pow(root->left->info, root->right->info);
+    case '^':return pow(eval(root->llink),eval(root->rlink));
     default:
       if(isalpha(root->info))
       {
-        printf("Enter No. to Replace %c : ", root->info);
+        printf("%c=",root->info);
         scanf("%f",&num);
         return num;
+        
       }
       else
-        return root->info - '0';
+      {
+        return root->info-'0';
+      }
+      
+      
 
   }
 }
-
-
 int main()
 {
-  char postfix[20];
+  char postfix[40];
   float res;
-  NODE root;
-  printf("Enter postfix expression:");
+  
+  NODE root=NULL;
+  printf("enter the postfix expression\n");
   scanf("%s",postfix);
-  root = create_tree(postfix);
-  res = eval(root);
-  printf("Result is %f\n",res);
+  root=create_tree(postfix);
+  printf("the expression tree is \n");
+  display(root,1);
+  res=eval(root);
+  printf("result:%f\n",res);
+
 }
