105d186d3ac8069b5859e50daf1caec1cda6afc1
piyush97 <me@piyushmehta.com>
2017-12-04 23:14:01 +0530

    improved

--- DS/Trees/ExpressionTree.c
+++ DS/Trees/ExpressionTree.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<ctype.h>
 #include<math.h>
+#include<ctype.h>
 struct node
 {
   char info;
@@ -9,48 +9,44 @@ struct node
   struct node* rlink;
 };
 typedef struct node* NODE;
-NODE getnode()
+void display(NODE root,int spaces)
 {
-  NODE x=(NODE)malloc(sizeof(struct node));
-  return x;
-}
-void display(NODE root,int level)
-{
-  int i;
-  if(root==NULL)return;
-  display(root->rlink,level+1);
-  for(i=0;i<level;i++)printf(" ");
-  printf("%c\n",root->info);
-  display(root->llink,level+1);
-  
+    if(root!=NULL)
+    {
+    int i;
+    display(root->rlink, spaces + 3);
+    for( i = 0; i < spaces; i++ )
+    printf(" ");
+    printf("%c\n",root->info);
+    display(root->llink, spaces + 3);
+    return;
+    }
 }
 NODE create_tree(char postfix[])
 {
-  NODE temp,st[20];
+  NODE st[20];
   int i,k;
-  char symbol;
-  for(i=k=0;(symbol=postfix[i])!='\0';i++)
+  for(i=k=0;postfix[i]!='\0';i++)
   {
-    temp=getnode();
-    temp->rlink=temp->llink=NULL;
-    temp->info=symbol;
-    if(isalnum(symbol))
+    NODE nn=(NODE)malloc(sizeof(struct node));
+    nn->rlink=nn->llink=NULL;
+    nn->info=postfix[i];
+    if(isalnum(postfix[i]))
     {
-      st[k++]=temp;
-      
+      st[k++]=nn;
     }
     else
     {
-      temp->rlink=st[--k];
-      temp->llink=st[--k];
-      st[k++]=temp;
+      nn->rlink=st[--k];
+      nn->llink=st[--k];
+      st[k++]=nn;
     }
   }
   return st[--k];
 }
-float eval(NODE root)
+int eval(NODE root)
 {
-  float num;
+  int num;
   switch(root->info)
   {
     case '+':return eval(root->llink) + eval(root->rlink);
@@ -63,24 +59,19 @@ float eval(NODE root)
       if(isalpha(root->info))
       {
         printf("%c=",root->info);
-        scanf("%f",&num);
+        scanf("%d",&num);
         return num;
-        
       }
       else
       {
         return root->info-'0';
       }
-      
-      
-
   }
 }
 int main()
 {
   char postfix[40];
-  float res; 
-  
+  int res;
   NODE root=NULL;
   printf("enter the postfix expression\n");
   scanf("%s",postfix);
@@ -88,6 +79,5 @@ int main()
   printf("the expression tree is \n");
   display(root,1);
   res=eval(root);
-  printf("result:%f\n",res);
-
+  printf("result:%d\n",res);
 }
