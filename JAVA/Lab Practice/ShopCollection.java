da0a87abcd7754f32bf49223ce6b7f3267555908
piyush97 <me@piyushmehta.com>
2017-12-11 12:09:32 +0530

    ShopCollections Update

--- JAVA/Lab Practice/ShopCollection.java
+++ JAVA/Lab Practice/ShopCollection.java
@@ -18,5 +18,21 @@ public class ShopCollection
 	}
 
 	//for increasing points for customer
-}
+	set<Map.Entry<Integer,Integer>> set=hashhash.entrySet();
+	for (Map.Entry<Integer,Integer> MapEntry:set ) 
+	{
+
+		if((MapEntry.getEntry()>=105)&&	(MapEntry.getEntry()>=120))
+			 {
+    		 hm.put(me.getKey(), me.getValue()+20);
+    		  	 }
+    	 if((me.getKey()>= 125) && (me.getKey()<=200))
+    	 {
+    		 hm.put(me.getKey(), me.getValue()+30);
+    		  	 }
+    	 System.out.println("for Customer id "+me.getKey()+" credit points are "+me.getValue());
+     }
+     
+	}
+
 }
