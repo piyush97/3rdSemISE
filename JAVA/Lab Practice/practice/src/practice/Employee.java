4dabd3f13b59243a2f9431ad132a8e0c3a7bf5c4
piyush97 <me@piyushmehta.com>
2017-12-11 11:24:27 +0530

    commit

--- JAVA/Lab Practice/practice/src/practice/Employee.java
+++ JAVA/Lab Practice/practice/src/practice/Employee.java
@@ -1,5 +1,24 @@
 package practice;
-
+import java.util.*;
 public class Employee {
+	String name,qual;
+	int age,exp;
+	Employee(String n,String q,int a,int y)
+	{
+		name=n;
+		qual=q;
+		age=a;
+		exp=y;
+	}
+	int getExp()
+	{
+		return(exp);
+	}
+	void display()
+	{
+		System.out.println("Name is " +name);
+		 System.out.println("Age is " +age);
+		System.out.println("Qualifia")
+	}
 
 }
