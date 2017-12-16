adbbc2a23e6c767b974dc7ca64baeb30da3ffb28
piyush97 <me@piyushmehta.com>
2017-12-16 10:07:50 +0530

    postfix eval easier code

--- DS/misc/Postfix Eval.c
+++ DS/misc/Postfix Eval.c
@@ -1,82 +1,64 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
+int stack[20];
+int top = -1;
 
-#define ST_SIZE 20
-
-
-void push ( double item, int *top, double * s);
-char pop ( int *top, double * s);
-
-
-
-double op( char symbol, double op1 , double op2 )
+void push(int x)
 {
-	switch( symbol )
- 	{
-		case '+': return op1+op2;
-		case '-': return op1-op2;
-		case '*': return op1*op2;
-		case '/': return op1/op2;
-		case '$': 
-		case '^': return pow(op1,op2);
-		default: return 0;
-	}
+    stack[++top] = x;
 }
 
-
-
-void push ( double item, int *top, double * s)
+int pop()
 {
-	if( *top == ST_SIZE - 1 )
-	{
-		printf("Stack Overflow");
-		return;	
-	}
-	s[++(*top)] = item;
+    return stack[top--];
 }
 
-
-char pop ( int *top, double * s)
+int main()
 {
-	if( *top == -1 )
-	{
-		printf("Stack underflow");
-		return 0;	
-	}
-	return s[(*top)--];
+    char exp[20];
+    char *e;
+    int n1,n2,n3,num;
+    printf("Enter the expression :: ");
+    scanf("%s",exp);
+    e = exp;
+    while(*e != '\0')
+    {
+        if(isdigit(*e))
+        {
+            num = *e - 48;
+            push(num);
+        }
+        else
+        {
+            n1 = pop();
+            n2 = pop();
+            switch(*e)
+            {
+                case '+':
+                {
+                    n3 = n1 + n2;
+                    break;
+                }
+                case '-':
+                {
+                    n3 = n2 - n1;
+                    break;
+                }
+                case '*':
+                {
+                    n3 = n1 * n2;
+                    break;
+                }
+                case '/':
+                {
+                    n3 = n2 / n1;
+                    break;
+                }
+            }
+            push(n3);
+        }
+        e++;
+    }
+    printf("\nThe result of expression %s  =  %d\n\n",exp,pop());
+    return 0;
+
 }
-
-int isitdigit( char symbol )
-{
-	return (symbol >= '0' && symbol <='9');
-}
-
-
-
-
-int main ( void )
-{
-	double s[ST_SIZE], op1,op2, res;
-	char postfix[ST_SIZE],symbol;
-	int top=-1, i;
-	printf("Enter Postfix Expression:");
-	scanf("%s",postfix);
-	for( i = 0; postfix[i]; i++ )
-	{
-		symbol=postfix[i];
-		if( isitdigit(symbol) )
-			push( symbol-'0',&top,s);
-		else
-		{
-			op2 = pop(&top,s);
-			op1 = pop(&top,s);
-			res = op(symbol,op1,op2);
-			push( res , &top , s);
-		}
-	}
-	res = pop(&top,s);
-	printf("Value of expression is: %lf\n",res);
-	return 0;
-}
-
-
