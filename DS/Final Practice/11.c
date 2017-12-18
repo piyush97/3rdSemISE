27bab27b28ad981449e7ca1b323be1c05bb84896
piyush97 <me@piyushmehta.com>
2017-12-18 18:56:42 +0530

    practice

--- DS/Final Practice/11.c
+++ DS/Final Practice/11.c
@@ -15,4 +15,54 @@ void insertion(int item)
 	newnode->info=item;
 	newnode->left=newnode->right=NULL;
 	
+	if(root==NULL)
+	{
+		root=newnode;
+		return;
+	}
+	struct node* parent=NULL;
+	struct node* current=root;
+	while(current!=NULL)
+	{
+		parent=current;
+		if(item<=current->info)
+			current=current->left;
+		else
+			current=current->right;
+
+	}
+	if (item<=parent->info)
+	{
+		parent->left=newnode;
+	}
+	else
+		parent->right=newnode;
+	return;
+}
+void inOrder(struct node* root)
+{
+	if(root!=NULL)
+	{
+		inOrder(root->left);
+		printf("%d\n",root->info );
+		inOrder(root->right);
+	}
+}
+int main()
+{
+	int choice,item;
+	printf("\n Press 1 for insertion\n2. for inOrder");
+	scanf("%d",&choice);
+	switch(choice)
+	{
+		case 1:
+		printf("Enter the element\n");
+		scanf("%d",&item);
+		insertion(item);
+		break;
+		case 2:
+		inOrder(root);
+		break;
+	}
+	return 0;
 }
