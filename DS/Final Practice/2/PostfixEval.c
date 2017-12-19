f179296b731362388645e44ce46097f81b097780
piyush97 <me@piyushmehta.com>
2017-12-20 00:52:39 +0530

    postfix eval

--- DS/Final Practice/2/PostfixEval.c
+++ DS/Final Practice/2/PostfixEval.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include<ctype.h>
 int top=-1,i=0,s[30];
 int push(int el)
 {
@@ -12,33 +13,33 @@ int pop()
 }
 void main()
 {
-char p[30],ch;
-int op2,op1;
-printf("Enter the postfix expression\n");
-scanf("%s",p);
-for(i=0;p[i]!='\0';i++)
-{
-ch=p[i];
-if(isdigit(ch))
-push(ch-48);
-else
-{
-op2=pop();
-op1=pop();
-switch(ch)
-{
-case '+':push(op1+op2);break;
-case '-':push(op1-op2);break;
-case '*':push(op1*op2);break;
-case '/':push(op1/op2);break;
-case '^':push(pow(op1,op2));break;
-if(op2==0)
-printf("Divide by zero error");
-else
-push(op1/op2);
-break;
-}
-}
-}
-printf("Answer is %d\n",s[top]);
+	char p[30],ch;
+	int op2,op1;
+	printf("Enter the postfix expression\n");
+	scanf("%s",p);
+	for(i=0;p[i]!='\0';i++)
+	{
+		ch=p[i];
+		if(isdigit(ch))
+		push(ch-48);
+		else
+		{
+			op2=pop();
+			op1=pop();
+			switch(ch)
+			{
+				case '+':push(op1+op2);break;
+				case '-':push(op1-op2);break;
+				case '*':push(op1*op2);break;
+				case '/':push(op1/op2);break;
+				case '^':push(pow(op1,op2));break;
+				if(op2==0)
+					printf("Divide by zero error");
+				else
+					push(op1/op2);
+				break;	
+			}
+		}
+	}
+	printf("Answer is %d\n",s[top]);
 }
