9285530f2527e5a5ef5a4900f63a852e25e061b4
piyush97 <me@piyushmehta.com>
2017-12-20 00:49:28 +0530

    postfix eval

--- DS/Final Practice/2/PostfixEval.c
+++ DS/Final Practice/2/PostfixEval.c
@@ -1,83 +1,44 @@
 #include<stdio.h>
-#define stacksize 50
-#include<math.h>
 #include<stdlib.h>
-#include<ctype.h>
-struct stack
+#include<math.h>
+int top=-1,i=0,s[30];
+int push(int el)
 {
-    int top;
-    float items[50];
-};
-
-typedef struct stack STACK;
-void push(STACK *s, float ele)
-{
-    if(s->top==stacksize-1)
-        printf("stack is full\n");
-    else
-        s->items[++(s->top)]=ele;
+   s[++top]=el;
 }
-
-int empty(STACK *s)
+int pop()
 {
-    return(s->top==-1);
+   return s[top--];
 }
-
-float pop(STACK *s)
-{
-    return(s->items[s->top--]);
-}
-
-float evaluate(float op1, char op, float op2)
-{
-    switch(op)
-    {
-        case '+': return(op1+op2);
-        case '-': return(op1-op2);
-        case '/': return(op1/op2);
-        case '*': return(op1*op2);
-        case '^': return(pow(op1,op2));
-        default: printf("Invalid\n");
-        exit(0);
-    }
-}
-
 void main()
 {
-    STACK opstk;
-    char postfix[20], symb;
-    int i=0;
-    opstk.top=-1;
-    float op1,op2,result;
-    printf("Enter a postfix expression\n");
-    gets(postfix);
-    
-    while((symb=postfix[i++])!='\0')
-    {
-        if(isdigit(symb))
-            push(&opstk,((float)symb-'0'));
-        else
-        {
-            if (empty(&opstk))
-            {
-                printf("Invalid\n");
-                exit(0);
-            }
-            op2=pop(&opstk);
-            
-            if (empty(&opstk))
-            {
-                printf("Invalid\n");
-                exit(0);
-            }
-            op1=pop(&opstk);
-            result=evaluate(op1,symb,op2);
-            push(&opstk,result);
-        }
-    }
-    result=pop(&opstk);
-    if (!empty(&opstk))
-        printf("Invalid/Empty\n");
-    else 
-        printf("Result=%f\n",result);
+char p[30],ch;
+int op2,op1;
+printf("Enter the postfix expression\n");
+scanf("%s",p);
+for(i=0;p[i]!='\0';i++)
+{
+ch=p[i];
+if(isdigit(ch))
+push(ch-48);
+else
+{
+op2=pop();
+op1=pop();
+switch(ch)
+{
+case '+':push(op1+op2);break;
+case '-':push(op1-op2);break;
+case '*':push(op1*op2);break;
+case '/':push(op1/op2);break;
+case '^':push(pow(op1,op2));break;
+if(op2==0)
+printf("Divide by zero error");
+else
+push(op1/op2);
+break;
+}
+}
+}
+printf("Answer is %d\n",s[top]);
 }
