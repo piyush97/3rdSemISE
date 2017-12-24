bbd687e3672a6b0dd9d7b6167f22d7581a45e44e
piyush97 <me@piyushmehta.com>
2017-12-24 22:42:31 +0530

    odll

--- DS LAB SOLUTIONS FOR SEE/13/ordered doubly ll.c
+++ DS LAB SOLUTIONS FOR SEE/13/ordered doubly ll.c
@@ -1,78 +1,85 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+
 struct node
 {
-	int info;
-	struct node *left;
-	struct node *right;
-}*start=NULL;
-
-void create(int item)
+    int info;
+    struct node *left;
+    struct node *right;
+};
+typedef struct node* NODE;
+NODE start=NULL;
+void insert_order(int item)
 {
-	struct node *newnode;
-	struct node *temp;
-	newnode=(struct node*) malloc(sizeof(struct node));
-	newnode->info=item;
-	newnode->left=newnode->right=NULL;
-	if(start==NULL)
-	{
-		start=newnode;
-		return;
-	}
-	temp=start;
-	while(temp->right!=NULL)
-	{
-		temp=temp->right;
-	}
-	while(temp!=NULL)
-	{
-		if(temp->info<=item)
-		{
-			newnode->right=NULL;
-			temp->right=newnode;
-			newnode->left=temp;
-			return;
-		}
-	
-		else
-		{
-			temp=temp->left;
-
-		}
-	}
-	newnode->right=start;
-	start->left=newnode;
-	start=newnode;
-
+    NODE cur,prev,nn;
+    nn=(NODE)malloc(sizeof(struct node));
+    nn->info = item;
+    nn->left = NULL;
+    nn->right = NULL;
+if(start==NULL)
+{
+    start=nn;
+    return;
 }
-void display()
+else if(item<=start->info)
 {
-	struct node *temp;
-	temp=start;
-	while(temp!=NULL)
-	{
-		printf("\n%d \n",temp->info );
-		temp=temp->right;
-	}
+nn->right=start;
+start->left=nn;
+start=nn;
+return;
+}
+prev=NULL;
+cur=start;
+while(cur!=NULL && item>cur->info)
+{
+prev=cur;
+cur=cur->right;
+}
+if(cur!=NULL)
+{
+nn->right=prev->right;
+prev->right=nn;
+nn->left=prev;
+cur->left=nn;
+}
+else
+{
+    prev->right=nn;
+    nn->left=prev;
+}
+return;
+}
+void disp()
+{
+NODE cur;
+if(start==NULL)
+printf("empty\n");
+else
+{
+cur=start;
+while(cur!=NULL)
+{
+printf("%d ",cur->info);
+cur=cur->right;
+}
+}
 }
 int main()
 {
-	int choice,item;
-	while(choice!=3)
-	{
-		printf("\nEnter 1 for insertion 2 for display\n");
-		scanf("%d",&choice);
-		switch(choice)
-		{
-			case 1:
-			printf("Enter the number to insert\n");
-			scanf("%d",&item);
-			create(item);
-			break;
-
-			case 2:
-			display();
-			break;
-		}
-	}
+int ch,item;
+while(ch!=3)
+{
+printf("enter choice\n1:Insert\n2:Display\n3:Exit\n");
+scanf("%d",&ch);
+switch(ch)
+{
+case 1:printf("enter element\n");
+scanf("%d",&item);
+insert_order(item);
+break;
+case 2:disp();
+break;
+}
+}
+return 0;
 }
